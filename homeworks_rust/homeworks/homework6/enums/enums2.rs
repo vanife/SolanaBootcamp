d821cc2617eb4dd828606d941e8bba1662aa9574
Ivan Melnychuk <ivan.melnychuk@gmail.com>
2025-03-19 14:10:04 +0100

    homework_rust 6 done

--- homeworks_rust/homeworks/homework6/enums/enums2.rs
+++ homeworks_rust/homeworks/homework6/enums/enums2.rs
@@ -1,11 +1,13 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move {x: i64, y: i64 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
